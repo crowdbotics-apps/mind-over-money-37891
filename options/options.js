345f5c5d90a2f1f5514a256032c68c592f484ce6
Crowdbotics <team@crowdbotics.com>
2022-12-14 01:15:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "freefall_37891",
+  url: "https://freefall_37891.botics.co",
+  api: "https://freefall_37891.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
